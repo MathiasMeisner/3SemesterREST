3c1f3d427e7f977442eebe9fb291a39324f166d6
MathiasMeisner <62882868+MathiasMeisner@users.noreply.github.com>
2021-05-10 09:49:24 +0200

    Tilføjet DateTime og Username

--- 3SemesterREST/Models/Booking.cs
+++ 3SemesterREST/Models/Booking.cs
@@ -8,8 +8,9 @@ namespace _3SemesterREST.Models
     public class Booking
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string Username { get; set; }
         public string LicensePlate { get; set; }
+        public DateTime BookTime { get; set; }
 
     }
 }
