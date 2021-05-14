77a8e387392c158312fdc459ac102bdd610b5a32
JohnEnevoldsen <62882814+JohnEnevoldsen@users.noreply.github.com>
2021-05-14 10:20:51 +0200

    Test til Car virker nu

--- 3SemesterRESTTests/Manager/CarManagerTests.cs
+++ 3SemesterRESTTests/Manager/CarManagerTests.cs
@@ -29,35 +29,50 @@ namespace _3SemesterREST.Manager.Tests
             CarManager manager = new CarManager(_context);
             List<Car> allCars = manager.GetAllCars().ToList();
 
-            
+            string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            string numbers = "0123456789";
+            var stringChars = new char[7];
+            var random = new Random();
+
+            for (int i = 0; i < 2; i++)
+            {
+                stringChars[i] = chars[random.Next(chars.Length)];
+            }
+            for (int i = 2; i < 7; i++)
+            {
+                stringChars[i] = numbers[random.Next(numbers.Length)];
+            }
+
+            string licensePlateString = new String(stringChars);
+
             // Add
-            Car data = new Car { Color = "Red", LicensePlate = "TestPlate" };
+            Car data = new Car { Color = "Red", LicensePlate = licensePlateString };
             Car newCar = manager.AddCar(data);
             Assert.IsTrue(newCar.Id > 0);
             Assert.AreEqual(data.Color, newCar.Color);
             Assert.AreEqual(data.LicensePlate, newCar.LicensePlate);
-            /*
-            Car nullModelData = new Car { ColorOfCar = "Dette er fra test"};
-            //Assert.ThrowsException<Exception>(() => manager.Add(nullModelData));
-
+            
+            Car nullModelData = new Car();
+            Assert.ThrowsException<CarException>(() => manager.AddCar(nullModelData));
+            
             // GetById
             Car carById = manager.GetCarById(newCar.Id);
             Assert.AreEqual(newCar.Id, carById.Id);
-            Assert.AreEqual(newCar.ColorOfCar, carById.ColorOfCar);
-            Assert.AreEqual(newCar.IsIn, carById.IsIn);
+            Assert.AreEqual(newCar.Color, carById.Color);
+            Assert.AreEqual(newCar.LicensePlate, carById.LicensePlate);
 
             Assert.IsNull(manager.GetCarById(newCar.Id + 1));
-
+            
             // Update
-            Car updates = new Car { ColorOfCar = "Volvo", IsIn = 1 };
+            Car updates = new Car { Color = "Volvo" };
             int id = newCar.Id;
             Car updatedCar = manager.UpdateCar(id, updates);
             Assert.AreEqual(id, updatedCar.Id);
-            Assert.AreEqual(updates.IsIn, updatedCar.IsIn);
+            Assert.AreEqual(updates.Color, updatedCar.Color);
 
             Assert.IsNull(manager.UpdateCar(id + 1, updates));
-            //Assert.ThrowsException<CarException>(() => manager.Update(id, nullModelData));
-           
+            //Assert.ThrowsException<CarException>(() => manager.UpdateCar(id, nullModelData));
+            /*
             //Delete all
             foreach(var d in manager.GetAllCars().ToList())
             {
