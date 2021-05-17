efb742a9c8fea42983b2a24fafa7c200732a43cf
NikolajHK <niko3009@edu.easj.dk>
2021-05-17 10:14:14 +0200

    ParkingBooths

--- WebPage/main.js
+++ WebPage/main.js
@@ -1,14 +1,19 @@
-const baseUri = "https://localhost:44350/api/bookings"
+const bookingUri = "https://localhost:44350/api/bookings"
+const parkingBoothUri = "https://localhost:44350/api/parkingbooths"
+
 
 Vue.createApp({
     data() {
         return {
+            parkingBooths: [],
+            parkingBooth: null,
+            parkingBooth: { isBooked: "", isOccupied: "", parkinglotId: "" },
             bookings: [],
-            oneBooking: null,
-            error: null,
-            id: "",
+            Booking: null,
             Booking: { username: "", licensePlate: "", bookTime: "" },
             Addbooking: "",
+            error: null,
+            id: "",
             LicensePlate: "",
             SingleLicensePlate: null,
         }
@@ -16,13 +21,14 @@ Vue.createApp({
     },
     async created() {
         console.log("created method called")
-        this.helperGetPosts(baseUri)
+        this.helperGetPosts(bookingUri)
+        this.helperGetPosts(parkingBoothUri)
     },
     methods: {
         resetList() {
             this.bookings = [],
                 this.error = null
-            this.oneBooking = null
+            this.Booking = null
         },
         async getByUserId(uid) {
             if (uid == null || uid == "") {
@@ -30,17 +36,31 @@ Vue.createApp({
                 this.bookings = []
                 console.log(uid)
             } else {
-                const uri = baseUri + "/" + uid
+                const uri = bookingUri + "/" + uid
                 console.log(uri)
                 const response = await axios.get(uri)
-                this.oneBooking = response.data
+                this.Booking = response.data
                 this.helperGetPosts(uri)
             }
             console.log(this.bookings)
         },
+        async getByParkingBoothId(uid) {
+            if (uid == null || uid == "") {
+                this.error = "No user id"
+                this.parkingBooths = []
+                console.log(uid)
+            } else {
+                const uri = parkingBoothUri + "/" + uid
+                console.log(uri)
+                const response = await axios.get(uri)
+                this.parkingBooth = response.data
+                this.helperGetPosts(uri)
+            }
+            console.log(this.parkingBooths)
+        },
 
         async getByLicensePlate(LicensePlate){
-            const url = baseUri + "/LicensePlate/" + LicensePlate
+            const url = bookingUri + "/LicensePlate/" + LicensePlate
             try{
                 response = await axios.get(url)
                 this.SingleLicensePlate = await response.data
@@ -53,21 +73,23 @@ Vue.createApp({
             try {
                 const response = await axios.get(uri)
                 this.bookings = await response.data
+                this.parkingBooths = await response.data
                 this.error = null
             } catch (ex) {
                 this.bookings = []
+                this.parkingBooths = []
                 this.error = ex.message
                 console.log(ex)
             }
         },
 
         getAllBookings(){
-            this.helperGetPosts(baseUri)
+            this.helperGetPosts(bookingUri)
         },
 
         async AddBooking() {
             try {
-                response = await axios.post(baseUri, this.Booking)
+                response = await axios.post(bookingUri, this.Booking)
                 this.Addbooking = "response" + response.status + " " + response.statusText
                 console.log(this.Booking)
                 //console.log(Booking.id)
