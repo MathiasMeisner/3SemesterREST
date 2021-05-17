d177f1107980b1ef0e701a7952c5807dd18d5930
SorenHersomNielsen <hersom@ellekrattet.dk>
2021-05-17 14:13:35 +0200

    test af Antal ledige båse

--- 3SemesterRESTTests/Manager/UITest.cs
+++ 3SemesterRESTTests/Manager/UITest.cs
@@ -28,7 +28,7 @@ namespace _3SemesterRESTTests.Manager
         }
 
         [TestMethod]
-        public void TestWebPage()
+        public void TestOfproductownerpage()
         {
             driver.Navigate().GoToUrl("file:///C:/Users/herso/OneDrive/Skrivebord/3.%20semester/3SemesterREST/WebPage/ProductOwner.html");
             Assert.AreEqual("Document", driver.Title);
@@ -50,6 +50,18 @@ namespace _3SemesterRESTTests.Manager
 
             Assert.AreEqual("", Text);
 
+        }
+
+        [TestMethod]
+        public void Testofkunde()
+        {
+            driver.Navigate().GoToUrl("file:///C:/Users/herso/OneDrive/Skrivebord/3.%20semester/3SemesterREST/WebPage/index.html");
+            Assert.AreEqual("Ledig Parkeringsplads", driver.Title);
+
+            IWebElement Output = driver.FindElement(By.Id("LotsCounter"));
+            string Text = Output.Text;
+
+            Assert.AreNotEqual("Et eller andet Antal ledige båse: 0", Text);
         }
     }
 }
