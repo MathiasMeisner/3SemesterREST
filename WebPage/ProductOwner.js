34c6d3a2bb4eb954a2b5b6fa7b1dc40a54392327
SorenHersomNielsen <hersom@ellekrattet.dk>
2021-05-18 10:46:25 +0200

    done med colorofcar i js

--- WebPage/ProductOwner.js
+++ WebPage/ProductOwner.js
@@ -1,4 +1,5 @@
 const baseuri = "https://localhost:44350/api/parkinglots"
+const uriCarcolor = "https://localhost:44350/api/cars/CarColor"
 
 Vue.createApp({
     data() {
@@ -6,7 +7,8 @@ Vue.createApp({
             singleCount: null,
             day: 0,
             month: 0,
-            year: 0
+            year: 0,
+            SingleColor: null
 
         }
     },
@@ -16,13 +18,27 @@ Vue.createApp({
             try {
                 const response = await axios.get(newuri)
                 this.singleCount = await response.data
-                if(this.singleCount == 0){
+                if (this.singleCount == 0) {
                     this.singleCount = "There is no data for this day "
                 }
             }
             catch (ex) {
                 alert(ex.message)
             }
+        },
+        async GetCarColor(Color) {
+            const newuri = uriCarcolor + "/" + Color
+            try {
+                const response = await axios.get(newuri)
+                this.SingleColor = await response.data
+                if (this.SingleColor == 0) {
+                    this.SingleColor = "Det er ingen biler med denne ønskede farve: " + Color
+                }
+            }
+            catch (ex) {
+                alert(ex.message)
+            }
         }
+
     }
 }).mount("#app")
