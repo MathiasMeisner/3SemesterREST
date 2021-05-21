cf318e9ccc891a3b1441f70705cc4f68559b4dda
NikolajHK <niko3009@edu.easj.dk>
2021-05-21 12:10:31 +0200

    swagger virker uden for development(IDE) ved ændring af startup.cs

--- 3SemesterREST/Startup.cs
+++ 3SemesterREST/Startup.cs
@@ -80,9 +80,9 @@ namespace _3SemesterREST
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "_3SemesterREST v1"));
             }
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "_3SemesterREST v1"));
 
             app.UseHttpsRedirection();
 
